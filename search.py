711abc6f375ea8988ea74ac677607fa07e5e78b2
Ernest Mamba <ebmamba@shoprite.co.za>
2023-12-11 17:17:26 +0200

    fix: resolving bugs in the algorithms

--- search.py
+++ search.py
@@ -4,15 +4,35 @@ from typing import Any
 class SearchAlg:
     @staticmethod
     def linear_search(numbers: int, search_target: Any) -> int:
+        '''
+            linear search algorithm
+        '''
+        if not numbers:
+            return -1
+        if not isinstance(search_target, int) or not all(isinstance(num, int) for num in numbers):
+            raise TypeError(
+                "Search target and list must be an of type integers")
+
         size = len(numbers)
         for index in range(size):
             if numbers[index] == search_target:
                 return index
+        return -1
 
     @staticmethod
     def binary_search(numbers: list, target: Any) -> int:
+        '''
+            binary search algorithm
+        '''
+        if not numbers:
+            return -1
+        if not isinstance(target, int) or not all(isinstance(num, int) for num in numbers):
+            raise TypeError("Search target and list must be integers")
+
         start = 0
         end = len(numbers)-1
+        sorted(numbers)  # enure to sort the array before searching
+
         while start <= end:
             mid = start + (end-start)//2
             if numbers[mid] == target:
--- sort.py
+++ sort.py
@@ -2,23 +2,34 @@ from typing import Any
 
 
 class SortAlg:
-
     @staticmethod
-    def bubble_sort(numbers: int) -> None:
+    def bubble_sort(numbers: list) -> None:
+        '''
+            bubble sort algorithm
+        '''
+        if not numbers or not all(isinstance(num, int) for num in numbers):
+            return
         n = len(numbers)
+        isswaped: bool = False
         for i in range(n-1):
             for j in range(0, n-i-1):
                 if numbers[j] > numbers[j + 1]:
+                    isswaped = True
                     numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+            if not isswaped:
+                return  # list is already sorted
         return
 
     @staticmethod
-    def insertion_sort(array):
-        for i in range(1, len(array)):
-            key_item = array[i]
+    def insertion_sort(numbers: list) -> None:
+        '''
+            insertion sort algorithm
+        '''
+        for i in range(1, len(numbers)):
+            key_item = numbers[i]
             j = i - 1
-            while j >= 0 and array[j] > key_item:
-                array[j + 1] = array[j]
+            while j >= 0 and numbers[j] > key_item:
+                numbers[j + 1] = numbers[j]
                 j -= 1
-            array[j + 1] = key_item
+            numbers[j + 1] = key_item
         return
